618bb8f8e7437277bcf76aa903c7ab9f848f6429
Jorrit van der Plaats <jvdplaats@hotmail.com>
2023-04-13 22:07:52 +0200

    bgimage not visible yet

--- src/components/Testimonios.js
+++ src/components/Testimonios.js
@@ -1,8 +1,48 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import "./Testimonios.scss";
 
 const Testimonios = () => {
-  return <div className="testimoniosContainer"></div>;
+  const [bgimage, setBgimage] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://silbodeelhierro.com//wp-json/wp/v2/media?per_page=1&search=bg"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let background = [];
+        data.map((img) => {
+          // console.log(img);
+          background = [
+            <img
+              t={img.id}
+              src={img.link}
+              alt={img.title.rendered}
+              height={150}
+              width={100}
+              key={img.id}
+            />,
+          ];
+        });
+        setBgimage(background);
+      });
+  }, []);
+
+  const myStyle = {
+    backgroundImage: { bgimage },
+    width: "100vw",
+    margin: "auto",
+    height: "300px",
+    backgroundSize: "cover",
+  };
+
+  return (
+    <div
+      // className="testimoniosContainer"
+      style={myStyle}
+    ></div>
+  );
 };
 
 export default Testimonios;
